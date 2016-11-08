acfcdab1775c07fc9912e689db7610457077aaac
Brandon Risberg <brandon@betterworks.com>
2016-11-07 17:33:07 -0800

    wip flamewreath

--- wow/wow.py
+++ wow/wow.py
@@ -8,10 +8,16 @@ class WowCog:
         self.bot = bot
 
     @commands.command()
-    async def flamewreath(self):
+    async def flamewreathtest(self):
         """This does stuff!"""
 
         await self.bot.say("I will not move when Flame Wreath is cast!")
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def flamewreath(self, ctx):
+        """I will not move when Flame Wreath is cast!"""
+        url = "https://www.youtube.com/watch?v=gcA6y7sxKcA"
+        await ctx.invoke(self.play, url_or_search_terms=url)
 
 def setup(bot):
     bot.add_cog(WowCog(bot))

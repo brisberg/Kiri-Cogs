725440cc28870da01d3b34c827fa9f685822a0f8
Brandon Risberg <brandon@betterworks.com>
2016-11-11 01:23:30 -0800

    reactions cog

---
+++ reactions/reactions.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+
+
+class ReactionsCog:
+    """Custom Cog that had commands for Reaction sounds"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def _play(self, url, ctx):
+        """Helper for aliasing Play in the Audio module"""
+        audio = self.bot.get_cog('Audio')
+        if not audio:
+            await self.bot.say("Audio module required. Load with: {}load audio".format(ctx.prefix))
+            return
+        await ctx.invoke(audio.play, url_or_search_terms=url)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def fail(self, ctx):
+        """You have failed horribly!"""
+        _play("https://www.youtube.com/watch?v=1ytCEuuW2_A", ctx)
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def goofy(self, ctx):
+        """Goofy yell!"""
+        _play("https://www.youtube.com/watch?v=MUL5w91dzbo", ctx)
+
+
+def setup(bot):
+    bot.add_cog(WowCog(bot))
